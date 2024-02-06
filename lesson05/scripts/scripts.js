d7dd43cba7657d04b19e56dae71564ac12b19107
aribyui <ari22008@byui.edu>
2024-02-06 00:02:49 -0600

    book of mormon favorite chapters application added

---
+++ lesson05/scripts/scripts.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("#favchap");
+const addChapterBtn = document.querySelector("button");
+const unorderedList = document.querySelector("#list");
+
+addChapterBtn.addEventListener("click", () => {      
+  if (input.value != "") {
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    
+    li.textContent = input.value;
+    deleteBtn.textContent = "❌";
+  
+    // Element: append() method - http://tinyurl.com/2fnkswxd
+    // Element.append() can append several nodes and strings, 
+    // whereas Node.appendChild() can only append one node.
+    li.appendChild(deleteBtn);
+    unorderedList.appendChild(li);
+
+    input.focus();
+    input.value = "";    
+  
+    deleteBtn.addEventListener("click", () => {
+      unorderedList.removeChild(li);
+      input.focus();
+      input.value = "";
+    }); 
+  } else {
+    input.focus();
+    alert("error");
+  }
+});
