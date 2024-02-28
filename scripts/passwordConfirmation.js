d1177e4746b78648f6d2e520bfa114cf2a12bfc7
aribyui <ari22008@byui.edu>
2024-02-28 09:37:32 -0600

    form.html added

---
+++ scripts/passwordConfirmation.js
@@ -0,0 +1,18 @@
+const password = document.querySelector("#userPassword");
+const passwordConfirmation = document.querySelector("#userPasswordConfirmation");
+const message = document.querySelector("#message");
+
+passwordConfirmation.addEventListener("focusout", checkSame);
+
+function checkSame() {
+  if (password.value !== passwordConfirmation.value) {
+    message.textContent = "Passwords do not match. Try again"
+    message.style.display = "block";
+    password.value = "";
+    passwordConfirmation.value = "";
+    password.focus();
+  } else {
+    message.style.display = "none";
+    message.style.color = "#171717";
+  }
+}
