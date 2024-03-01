b06a46c346618558d72ad9fda362c1bc639a05bd
aribyui <ari22008@byui.edu>
2024-03-01 10:57:44 -0600

    dateTime variable added to getDates.js file

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,5 +1,10 @@
+// all pages
 const lastModified = document.lastModified;
 const currentYear = new Date();
+const dateTime = Date.now();
 
 document.querySelector("#current-year").textContent = currentYear.getFullYear();
 document.querySelector("#last-modified").textContent = lastModified;
+
+// join page
+document.querySelector("#date-time").setAttribute("value", dateTime);
