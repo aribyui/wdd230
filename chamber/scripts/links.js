02a2783bfd01e5e921a2027555422fc10dcd0393
aribyui <ari22008@byui.edu>
2024-03-08 00:16:42 -0600

    directory html, css and js updated

--- chamber/scripts/links.js
+++ chamber/scripts/links.js
@@ -1,17 +1,36 @@
 const baseURL = "https://aribyui.github.io/wdd230/";
 const linksURL = "https://aribyui.github.io/wdd230/chamber/data/members.json";
 const gridButton = document.querySelector("#grid");
-const gridList = document.querySelector("#list");
-const article = document.querySelector("article");
+let gridList = document.querySelector("#list");
+let article = document.querySelector("article");
+
+gridButton.classList.add("borderButton");
+gridList.classList.add("borderButtonNoMargin");
 
 gridButton.addEventListener("click", () => {
   article.classList.add("grid");
   article.classList.remove("list");
+
+  if (gridList.classList.contains("borderButton")) {
+    gridList.classList.remove("borderButton");
+    gridList.classList.add("borderButtonNoMargin");
+    gridButton.classList.remove("borderButtonNoMargin")
+    gridButton.classList.add("borderButton");    
+  }
+
 });
 
 gridList.addEventListener("click", () => {
   article.classList.add("list");
   article.classList.remove("grid");
+
+  if (gridButton.classList.contains("borderButton")) {
+    gridButton.classList.remove("borderButton");
+    gridButton.classList.add("borderButtonNoMargin");
+    gridList.classList.remove("borderButtonNoMargin");
+    gridList.classList.add("borderButton"); 
+  }
+
 });
 
 async function getMembers() {
@@ -29,13 +48,14 @@ const displayMembers = (members) => {
     // console.log(member.logo[0].title) // se imprime en consola para depurar
 
     const section = document.createElement("section");
-    section.innerHTML = 
-      `<p>${member.name}</p>      
+    section.innerHTML =
+      `<h2>${member.name}</h2>
+      <p>${member.name}</p>
       <img src="${member.logo[0].url}" title="${member.logo[0].title}" width="150" height="150" loading="lazy"></img>
       <p>${member.membershipLevel}</p>
       <p>${member.address}, ${member.zipcode}</p>
       <p>${member.phone}</p>
-      <a href="${member.website}" target="_blank">${member.website}</a>`;
+      <a href="${member.website}" target="_blank">Site</a>`;
 
     article.appendChild(section);
   });
