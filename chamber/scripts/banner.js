39cec917f8d762f80934eaab359f9062464eea78
aribyui <ari22008@byui.edu>
2024-03-15 01:23:10 -0600

    index.html updated

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,13 +1,18 @@
 const banner = document.querySelector(".banner");
 const bannerMessage = document.createElement("p");
+const deleteButton = document.createElement("span");
+
 bannerMessage.textContent = "We invite you to join us for the Chamber of Commerce meet and greet on Wednesday at 7:00 p.m";
+deleteButton.textContent = "❌";
+
 banner.appendChild(bannerMessage);
+banner.appendChild(deleteButton);
 
 const currentDay = new Date().getDay();
 console.log(currentDay);
 
-if (currentDay === 4 || currentDay === 2) {
-  banner.classList.toggle("showBanner");
-} else {
+currentDay === 5 || currentDay === 2 || currentDay === 3 ? banner.classList.toggle("showBanner") : banner.classList.toggle("closeBanner");
+
+deleteButton.addEventListener("click", () => {
   banner.classList.toggle("closeBanner");
-}
+});
