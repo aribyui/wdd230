000e19dea771d7299d0b2699fe6afec858a38e68
aribyui <ari22008@byui.edu>
2024-03-19 11:37:15 -0600

    members.json updated

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -38,6 +38,7 @@ function displayMembers(members) {
     const h3 = document.createElement("h3");
     const img = document.createElement("img");
     const mLevel = document.createElement("p");
+    const stars = document.createElement("span");
     const address = document.createElement("p");
     const phone = document.createElement("p");    
     const website = document.createElement("a");
@@ -48,15 +49,18 @@ function displayMembers(members) {
     img.setAttribute("width", "150");
     img.setAttribute("height", "150");
     img.setAttribute("loading", "lazy");  
-    mLevel.textContent = members[randomInt].membershipLevel;    
+    mLevel.textContent = members[randomInt].membershipLevel;   
+    stars.textContent = members[randomInt].stars; 
     address.textContent = members[randomInt].address;
     phone.textContent = members[randomInt].phone;
     website.textContent = "Website";
     website.setAttribute("href", members[randomInt].website);
+    website.setAttribute("target", "_blank");
     
     section.appendChild(h3)
     section.appendChild(img)
     section.appendChild(mLevel);
+    section.appendChild(stars);
     section.appendChild(address);
     section.appendChild(phone);     
     section.appendChild(website)
