13cc007652b7f13c3adb0a8d9ba01825abbeb849
aribyui <ari22008@byui.edu>
2024-03-19 08:57:10 -0600

    links.js updated

--- chamber/scripts/links.js
+++ chamber/scripts/links.js
@@ -9,7 +9,7 @@ const article = document.querySelector("article");
 let data = null;
 
 // Este bloque if es detectado automáticamente cuando la página "index.html" se encuentra activa
-if (indexPage.includes("index.html")) {
+if (indexPage.includes("chamber/index.html")) {
 
   async function getMembers() {
     const response = await fetch(linksURL);
