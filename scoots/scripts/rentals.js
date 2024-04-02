aa084d9f2e79990082d183bf78b9477c946a5c64
aribyui <ari22008@byui.edu>
2024-04-02 10:24:16 -0600

    rentals.js updated url

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -1,4 +1,4 @@
-const rentalsUrl = "https://aribyui.github.io/wdd230/scooter/data/data.json";
+const rentalsUrl = "https://aribyui.github.io/wdd230/scoots/data/data.json";
 const prices = document.querySelector(".prices");
 
 async function getRentals() {
@@ -12,30 +12,28 @@ async function getRentals() {
 
 function displayRentals(data) {
   data.forEach((d) => {
-    const table = document.createElement("div");
-    const rentalType = document.createElement("p");
+    const div = document.createElement("div");
+    const table = document.createElement("table");
 
-    table.classList.add("table");
+    // table.classList.add("table");
 
-    rentalType.innerHTML = `
-    <p>Rental type</p>
-    <p>${d.rental_type}</p>
-    <p>Max persons</p>
-    <p>${d.max_persons}</p>
-    <p>Reservation</p>
-    <p>Half day</p>
-    <p>${d.reservation[0].half_day}</p>
-    <p>Full day</p>
-    <p>${d.reservation[0].full_day}</p>
-    <p>Half day</p>
-    <p>${d.walk_in[0].half_day}</p>
-    <p>Full day</p>
-    <p>${d.walk_in[0].full_day}</p>
-    `;
+    // rentalType.innerHTML = `
+    // <p>Rental type</p>
+    // <p>${d.rental_type}</p>
+    // <p>Max persons</p>
+    // <p>${d.max_persons}</p>
+    // <p>Reservation</p>
+    // <p>Half day</p>
+    // <p>${d.reservation[0].half_day}</p>
+    // <p>Full day</p>
+    // <p>${d.reservation[0].full_day}</p>
+    // <p>Half day</p>
+    // <p>${d.walk_in[0].half_day}</p>
+    // <p>Full day</p>
+    // <p>${d.walk_in[0].full_day}</p>
+    // `;
 
-    table.appendChild(rentalType);
-
-    prices.appendChild(table);
+    // prices.appendChild(table);
   });
 }
 
